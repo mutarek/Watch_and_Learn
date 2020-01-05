5829a37cb82aec467f69c0eb15a5ceca281d6faf
mohiuddintarek <41743928+mohiuddin-Tarek@users.noreply.github.com>
2020-01-05 12:40:25 +0600

    Initial commit

---
+++ app/src/main/java/com/bdsoftwaresolution/watchandlearn/Activities/SIgnUpActivity.java
@@ -0,0 +1,16 @@
+package com.bdsoftwaresolution.watchandlearn.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bdsoftwaresolution.watchandlearn.R;
+
+public class SIgnUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
